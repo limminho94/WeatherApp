e646227eb62104b0ea6198ddb74c97bba2fe14ec
limminho94 <github.com/limminho94>
2024-12-02 20:13:26 +0900

    초기 커밋.

---
+++ ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using weatherApp.Model;
+
+namespace weatherApp.ViewModel.Helpers
+{
+    class AccuWeatherHelper
+    {
+        public const string API_KEY = "u1exIoud0BZrm5tAq26Zjcabu24RbGyj";
+
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+
+        public const string LANGUAGE = "ko-kr";
+
+        public const string AUTOCOMPLETE_ENDPOINT =
+            "locations/v1/cities/autocomplete?apikey={0}&q={1}&language={2}";
+
+        public const string CURRENT_CONDITIONS_ENDPOINT =
+            "currentconditions/v1/{0}?apikey={1}&language={2}";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            // API Json 결과에서 City 객체 타입 데이터를 List에 담습니다.
+            List<City> cities = new List<City>();
+
+            // API 요청 URL 을 설정합니다.
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query, LANGUAGE);
+
+            // HttpClient 에서
+            using (HttpClient clnt = new HttpClient())
+            {
+                // url로 API 요청을 request 하고, reponse 값을 cities 리스트에 할당합니다.
+                var res = await clnt.GetAsync(url);
+                string json = await res.Content.ReadAsStringAsync();
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditions(string citikey)
+        {
+            // API Json 결과에서 CurrentConditions 객체 타입 데이터를 List에 담습니다.
+            CurrentConditions currentConditions = new CurrentConditions();
+
+            // API 요청 URL 을 설정합니다.
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_ENDPOINT, citikey, API_KEY, LANGUAGE);
+
+            // HttpClient 에서
+            using (HttpClient clnt = new HttpClient())
+            {
+                // url로 API 요청을 request 하고, reponse 값을 CurrentConditions 리스트에 할당합니다.
+                var res = await clnt.GetAsync(url);
+                string json = await res.Content.ReadAsStringAsync();
+                currentConditions = JsonConvert.DeserializeObject<List<CurrentConditions>>(json).FirstOrDefault();
+            }
+            return currentConditions;
+        }
+    }
+}
---
+++ ViewModel/WeatherVM.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using weatherApp.Model;
+
+namespace weatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                Selectedcity = new City
+                {
+                    LocalizedName = "서울"
+                };
+
+                currentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 21
+                        }
+                    }
+                };
+            }
+        }
+
+        //[1] 시나리오: 도시 검색을 요청(query)한다.
+        private string query;
+        public string Query
+        {
+            get { return query; }
+            // [2] Property가 변경되면, 
+            set
+            {
+                query = value;
+                // OnPropertyChanged 함수에서
+                OnPropertyChanged(nameof(Query));
+            }
+        }
+
+        //[1] 시나리오: CurrentConditions 현재 날씨 정보가 업데이트된다.
+        private CurrentConditions currentCondition;
+        public CurrentConditions currentConditions
+        {
+            get { return currentCondition; }
+            // [2] Property가 변경되면,
+            set
+            {   
+                currentCondition = value;
+                // OnPropertyChanged 함수에서
+                OnPropertyChanged(nameof(currentConditions));
+            }
+        }
+        //[1] 시나리오: City 정보가 업데이트 된다.
+        private City selectedcity;
+
+        public City Selectedcity
+        {
+            get { return selectedcity; }
+            // [2] Property가 변경되면,
+            set
+            {
+                selectedcity = value;
+                // OnPropertyChanged 함수에서
+                OnPropertyChanged(nameof(Selectedcity));
+            }
+        }
+
+        // [4] 이벤트 핸들러가 동작하면, 
+        // XAML에 바인딩 되어있는 모든 UI컨트롤에 해당 query 값이 업데이트 된다.
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            // [3] PropertyChanged 이벤트를 실행한다.
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
